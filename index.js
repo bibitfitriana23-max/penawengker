3b33d6227861b0d71d85a1d70d0f3ab6cbd83a15
bibitfitriana23-max <bibitfitriana23@gmail.com>
2026-02-27 14:14:18 +0700

    saya ngubah pakai cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose.connect("mongodb://bibit3_db_user:admin123@ac-txqxvt5-shard-00-00.f5tjq
 //});
 
 app.use(express.json());
-APP.use(cors());
+app.use(cors());
 
 app.use('/notes', notes);
 
